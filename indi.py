fcc0191f87900eb77236c58f373b3531ede866ad
devmallett <34248685+devmallett@users.noreply.github.com>
2020-04-25 00:42:23 -0400

    Shaping Data

--- indi.py
+++ indi.py
@@ -11,9 +11,10 @@ CATEGORIES = ["Dog", "Cat"]
 for category in CATEGORIES:
     path = os.path.join(DATADIR, category)
     for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path, img),cv2.IMREAD_GRAYSCALE)
+        # img_array = cv2.imread(os.path.join(path, img),cv2.IMREAD_GRAYSCALE)
+        img_array = cv2.imread(os.path.join(path, img))
         plt.imshow(img_array, cmap="gray")
         plt.show()
         break
     break
-print(img_array)
+print(img_array.shape)
