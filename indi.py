7ecedc826e2ea443f5a445ba088b2a2abf4aab23
devmallett <34248685+devmallett@users.noreply.github.com>
2020-04-25 17:11:29 -0400

    Classifying training data

--- indi.py
+++ indi.py
@@ -17,4 +17,33 @@ for category in CATEGORIES:
         plt.show()
         break
     break
-print(img_array.shape)
+# print(img_array.shape)
+
+IMG_SIZE = 50
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap = 'gray')
+# plt.show()
+
+
+# Crating training data set 
+
+training_data = []
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+    for img in os.listdir(path):
+        # img_array = cv2.imread(os.path.join(path, img),cv2.IMREAD_GRAYSCALE)
+        try:
+            img_array = cv2.imread(os.path.join(path, img))
+            new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+            training_data.append([new_array, class_num])
+        except Exception as e:
+            pass
+create_training_data()
+
+print(len(training_data))
+
+# balance your training data 
+
