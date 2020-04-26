44d2cd8b0c3c52f66444a148f5ebfdfb5ab4b5bf
devmallett <34248685+devmallett@users.noreply.github.com>
2020-04-26 17:30:03 -0400

    Shuffle of data
    While training a network you want to shuffle the data as the network can learn over time to identify a label based on its position in some list

--- indi.py
+++ indi.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
-
+import random
 # python indi.py
 
 DATADIR = "C:/Datasets/PetImages"
@@ -14,7 +14,7 @@ for category in CATEGORIES:
         # img_array = cv2.imread(os.path.join(path, img),cv2.IMREAD_GRAYSCALE)
         img_array = cv2.imread(os.path.join(path, img))
         plt.imshow(img_array, cmap="gray")
-        plt.show()
+        # plt.show()
         break
     break
 # print(img_array.shape)
@@ -43,7 +43,13 @@ def create_training_data():
             pass
 create_training_data()
 
-print(len(training_data))
 
-# balance your training data 
+# print(len(training_data))
 
+# balance your  data set  
+
+# shuffle data 
+random.shuffle(training_data)
+
+for sample in training_data[:10]:
+    print(sample[1])
