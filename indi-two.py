c5c2e090d3a5aa53499130e1b2e0b6d76039d028
devmallett <34248685+devmallett@users.noreply.github.com>
2020-04-30 22:27:30 -0400

    Assigning
    Assigning numerical value to labela

--- indi-two.py
+++ indi-two.py
@@ -25,10 +25,33 @@ for some_image in CHUMPSKY:
 
 IMG_SIZE = 50
 new_devin = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-plt.imshow(new_devin, cmap="Blues")
+plt.imshow(new_devin, cmap="gray")
 plt.show()
 
-print(new_devin)
+# print(new_devin)
+
+training_data = []
+
+# Setting Classification as Numbers 
+def creating_training_data():
+    for some_image in CHUMPSKY:
+        path = os.path.join(DATADIR, some_image) 
+        class_num = CHUMPSKY.index(some_image)
+        for imgages in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, imgages), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num]) 
+            except Exception as e:
+                pass 
+creating_training_data()
+
+# print(len(training_data))
+
+
+    
+
+
 
 # Pick up at 6:44
 # https://www.youtube.com/watch?v=j-3vuBynnOE
