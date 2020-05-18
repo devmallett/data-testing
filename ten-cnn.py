761e1dd8e7c8e7bb49d4b951c8f1e10e5cc97fda
devmallett <34248685+devmallett@users.noreply.github.com>
2020-05-18 17:57:05 -0400

    Config Update
    Got gpu to work

--- ten-cnn.py
+++ ten-cnn.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
 # from tensorflow import ConfigProto, Session
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import pickle
@@ -11,10 +13,11 @@ from keras import backend as K
 # https://www.youtube.com/watch?v=WvoLTXIjBYU
 
 #Running Many GPUs at the same time
-# gpu_options = 
-# gpu_options = tf.compat.v1.ConfigProto.GPUoptions(per_process_gpu_memory_fraction=0.333)
-# # config = tf.compat.v1.ConfigProto(gpu_options = (per_process_gpu_memory_fraction=0.3))
-# sess = tf.compat.v1.ConfigProto.Session(config=gpu_options)
+config = ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.333
+session = InteractiveSession(config=config)
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+# sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 X = pickle.load(open("X.pickle", "rb")) #images
 y = pickle.load(open("y.pickle", "rb")) #labels
@@ -36,40 +39,37 @@ def swish_activation(x):
 # model = keras.Sequential([
 #     keras.layers.Conv2D((64), (3,3), input_shape = X.shape[1:]),
 #     keras.layers.Activation("selu"), #accuracy #.7343 
-#     # tf.keras.activations.swish_activation(X),
+# 
 #     keras.layers.MaxPooling2D(pool_size=(2,2)),
 #     # ------------------------------- end of layer 1
 #     keras.layers.Conv2D((64), (3,3)),
 #     keras.layers.Activation("selu"),
-#     # tf.keras.activations.swish_activation(X),
+# 
 #     keras.layers.MaxPooling2D(pool_size=(2,2)), #2 x 2 filter, derivative, used to reduce 
 #     # --------------------------------- end of layer 2
 #     keras.layers.Flatten(),
 #     keras.layers.Dense(64),
 #     keras.layers.Activation("selu"),
-#     # tf.keras.activations.swish_activation(X),
+# 
 #     keras.layers.Dense(1),
 #     keras.layers.Activation("sigmoid"),
 
 #     # when does one use a dense layer?
 # ])
 
-#accuracy 
+#accuracy 0.722 | 0.7699 | 0.7896
 model = keras.Sequential([
     keras.layers.Conv2D((64), (3,3), input_shape = X.shape[1:]),
-    keras.layers.Activation("relu"), #accuracy #.7343 
-    # tf.keras.activations.swish_activation(X),
+    keras.layers.Activation("relu"), #accuracy #.7343
     keras.layers.MaxPooling2D(pool_size=(2,2)),
     # ------------------------------- end of layer 1
     keras.layers.Conv2D((64), (3,3)),
     keras.layers.Activation("relu"),
-    # tf.keras.activations.swish_activation(X),
     keras.layers.MaxPooling2D(pool_size=(2,2)), #2 x 2 filter, derivative, used to reduce 
     # --------------------------------- end of layer 2
     keras.layers.Flatten(),
     keras.layers.Dense(64),
     keras.layers.Activation("relu"),
-    # tf.keras.activations.swish_activation(X),
     keras.layers.Dense(1),
     keras.layers.Activation("sigmoid"),
 
